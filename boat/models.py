9d57df525a15969fd755895a3a06a7ea8b52cc93
alexander <vertiporogov.alexandr@mail.ru>
2024-01-20 13:23:55 +0300

    commit 20.01.2024

--- boat/models.py
+++ boat/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=100, verbose_name='имя владеллца')
+    email = models.EmailField(verbose_name='почта владельца', unique=True)
+
+    def __str__(self):
+        return f'{self.email} ({self.name})'
+
+    class Meta:
+        verbose_name = 'владелец'
+        verbose_name_plural = 'владельцы'
+
+
+class Boat(models.Model):
+    name = models.CharField(max_length=50, verbose_name='название лодки')
+    year = models.PositiveSmallIntegerField(**NULLABLE, verbose_name='год выпуска')
+
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, verbose_name='владелец лодки')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'лодка'
+        verbose_name_plural = 'лодки'
+
+
+class BoatHistory(models.Model):
+    boat = models.ForeignKey(Boat, on_delete=models.CASCADE, verbose_name='лодка')
+    owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, verbose_name='владелец лодки', **NULLABLE)
+
+    start_year = models.PositiveSmallIntegerField(**NULLABLE, verbose_name='владел с')
+    stop_year = models.PositiveSmallIntegerField(**NULLABLE, verbose_name='владел по')
+
+    def __str__(self):
+        return f'{self.boat} {self.start_year}-{self.stop_year}'
+
+    class Meta:
+        verbose_name = 'история'
+        verbose_name_plural = 'история'
---
+++ boat/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from boat.apps import BoatConfig
+
+app_name = BoatConfig.name
+
+
+urlpatterns = [
+
+]
